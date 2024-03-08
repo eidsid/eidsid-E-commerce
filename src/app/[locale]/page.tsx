5b01af11796c7d5724d36476150e0a1f14ed0aa7
eid sayed <eidsiddiedis@gmail.com>
2024-03-08 20:06:50 +0200

    add best sales omponent and fix styles

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,20 +5,26 @@ import { getDict } from "../../../dictionaries/dictionaries";
 import { headers } from "next/headers";
 import FlashSales from "../components/FlashSales";
 import Categories from "../components/Categories";
+import BestSales from "../components/bestSales";
 export default async function Home() {
   const Local = getLocaleInServer(headers);
   const dectionary: any = await getDict(Local);
   const flashSlaesDectionary = dectionary.pages.index.sales;
   const CategorieDectionary = dectionary.pages.index.category;
   const sideMenuDectionary = dectionary.pages.index.sideMenu;
+  const BestSalesDectionary = dectionary.pages.index.bestSelling;
+
   return (
     <div className="container  m-auto  ">
       <div className="  flex   justify-between p-4   mx-auto   ">
         <Sidbar sideMenu={sideMenuDectionary} />
         <Banner />
       </div>
-      <FlashSales Local={Local} dic={flashSlaesDectionary} />
-      <Categories Local={Local} dic={CategorieDectionary} />
+      <div className="flex flex-col gap-8">
+        <FlashSales Local={Local} dic={flashSlaesDectionary} />
+        <Categories Local={Local} dic={CategorieDectionary} />
+        <BestSales Local={Local} dic={BestSalesDectionary} />
+      </div>
     </div>
   );
 }
