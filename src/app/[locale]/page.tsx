aca9f8664d865e178614cb743496a138705ef0b7
eid sayed <eidsiddiedis@gmail.com>
2024-03-09 14:42:59 +0200

    add OurProfucts component and pass favoritesIds to components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,6 +7,7 @@ import FlashSales from "../components/FlashSales";
 import Categories from "../components/Categories";
 import BestSales from "../components/bestSales";
 import SingleProductBanner from "../components/SingleProductBanner";
+import OurProfucts from "../components/ourproducts";
 export default async function Home() {
   const Local = getLocaleInServer(headers);
   const dectionary: any = await getDict(Local);
@@ -15,6 +16,9 @@ export default async function Home() {
   const sideMenuDectionary = dectionary.pages.index.sideMenu;
   const BestSalesDectionary = dectionary.pages.index.bestSelling;
   const singProductDectionary = dectionary.pages.index.banner;
+  const productsDectionary = dectionary.pages.index.products;
+  const favoritesIDs: string[] = ["1", "3", "2", " 4", "6", " 7", " 8"];
+
   return (
     <div className="container  m-auto  ">
       <div className="  flex   justify-between p-4   mx-auto   ">
@@ -22,10 +26,23 @@ export default async function Home() {
         <Banner />
       </div>
       <div className="flex flex-col gap-8">
-        <FlashSales Local={Local} dic={flashSlaesDectionary} />
+        <FlashSales
+          Local={Local}
+          dic={flashSlaesDectionary}
+          favoritesIDs={favoritesIDs}
+        />
         <Categories Local={Local} dic={CategorieDectionary} />
-        <BestSales Local={Local} dic={BestSalesDectionary} />
+        <BestSales
+          Local={Local}
+          dic={BestSalesDectionary}
+          favoritesIDs={favoritesIDs}
+        />
         <SingleProductBanner Local={Local} dic={singProductDectionary} />
+        <OurProfucts
+          favoritesIDs={favoritesIDs}
+          dic={productsDectionary}
+          Local={Local}
+        />
       </div>
     </div>
   );
