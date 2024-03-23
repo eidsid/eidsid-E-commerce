2f7234c96b26195cea3d3909caa36c8b0741fbf2
eid sayed <eidsiddiedis@gmail.com>
2024-03-23 17:09:04 +0200

    create Header Link

---
+++ src/app/components/header/HeaderLink.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function HeaderLink({
+  href,
+  text,
+}: {
+  href: string;
+  text: string;
+}) {
+  const pathname = usePathname();
+  const locale = pathname.split("/")[1];
+
+  return (
+    <Link
+      href={`/${locale}/${href}`}
+      className={`capitalize text-base max-2xl:text-sm ${
+        pathname.split("/")[2] === href
+          ? "underline underline-offset-4"
+          : pathname.split("/").length === 2 && href.length === 0
+          ? "underline underline-offset-4"
+          : ""
+      } `}
+    >
+      {text}
+    </Link>
+  );
+}
